a507215286b82b254d13b76fd85a2d06a04defc7
jbogner <boginator55@gmail.com>
2018-10-15 21:52:16 -0400

    Add capability to read from the prepackaged shrek.txt file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Pls drink bleach and kill yourself.");
+    // --snip--
+    let filename = "assets/sample_txts/shrek.txt";
+    println!("In file {}", filename);
+
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
