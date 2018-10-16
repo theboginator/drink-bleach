10fccc80db74eed50958b3c6954adac16bcf1da3
jbogner <boginator55@gmail.com>
2018-10-16 10:08:35 -0400

    Add capability to read from the prepackaged shrek.txt file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use std::env;
+use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
     // --snip--
+    let files = fs::read_dir("assets/sample_txts").unwrap(); //Get the sample texts directory
+    for file in files {
+        println!("Name: {}", path.unwrap().path().display()) //Print out each .txt file in the sample txts directory
+    }
+
+    println!("Enter the name of any of the above text files to use: (To use another file,\
+    place it in the \"assets/sample_txts\" folder and re-launch the application."); //Prompt the user to enter the name of a file to count words in
+
     let filename = "assets/sample_txts/shrek.txt";
     println!("In file {}", filename);
 
